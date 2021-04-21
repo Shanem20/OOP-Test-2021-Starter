1f000ce243d251c904447b8dc5588eb0c859717f
Shanem20 <c19480206@mytudublin.ie>
2021-04-21 20:10:41 +0100

    commit 1

---
+++ java/src/ie/tudublin/Note.java
@@ -0,0 +1,22 @@
+package ie.tudublin;
+
+
+public class Note {
+
+    private char note;
+
+    private int duration; 
+
+  // Create class constructor for Note class
+  public Note() {
+
+  }
+
+  public static void main(String[] args) {
+    Note myObj = new Note(); // Create an object of class Main (This will call the constructor)
+    System.out.println(myObj.note); // Print the value of x
+  }
+}
+
+
+}
--- java/src/ie/tudublin/ScoreDisplay.java
+++ java/src/ie/tudublin/ScoreDisplay.java
@@ -1,5 +1,6 @@
 package ie.tudublin;
 
+import java.util.*;
 import java.util.ArrayList;
 
 
@@ -7,9 +8,28 @@ import processing.core.PApplet;
 
 public class ScoreDisplay extends PApplet
 {
+	ArrayList<Note> notes = new ArrayList<Note>(); // Creates ArrayList object
 	String score = "DEFGABcd";
 	//String score = "D2E2F2G2A2B2c2d2";
 	//String score = "DEF2F2F2EFA2A2B2AFD2E2D2D2D2";
+
+
+	void loadScore()
+	{
+		// Split scores into indivudal arrays and test output using for-each loop
+		String[] arr = score.split("");
+		for(String character : arr)
+		{
+			System.out.print(character + " ");
+		}
+
+
+		for(String character : arr)
+		{
+			notes.add(character);
+		}
+	}
+
 	
 	public void settings()
 	{
@@ -19,8 +39,19 @@ public class ScoreDisplay extends PApplet
 		char c = '7'; // c holds the character 7 (55)
 		int i = c - '0'; // i holds the number 7 (55 - 48) 
 		println(i);
+	
 	}
 
+	void drawLines()
+	{
+		stroke(0);
+		line(45, 110, 940, 110);
+		line(45, 170, 940, 170);
+		line(45, 230, 940, 230);
+		line(45, 290, 940, 290);
+		line(45, 350, 940, 350);
+	}
+	
 	public void setup() 
 	{
 		
